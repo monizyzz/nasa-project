dc88daade8a70b6e46dbbb561d69a0e01bbe234e
bernazyzz <zebernardomoniz@gmail.com>
2025-08-28 22:31:11 +0100

    POST /launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
@@ -14,9 +16,23 @@ const launch = {
 launches.set(launch.flightNumber, launch);
 
 function getAllLaunches() {
-  return Array.from(launches.values())
+  return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      costumer: ["NASA", "Zero to Mastery"],
+      flightNumber: latestFlightNumber,
+    })
+  );
 }
 
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
