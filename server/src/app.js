a17c6dfa9104bedb49af92b0f99cd0e7f901803d
bernazyzz <zebernardomoniz@gmail.com>
2025-08-28 11:38:24 +0100

    logging requests with morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
 
@@ -12,12 +13,14 @@ app.use(
   })
 );
 
+app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetsRouter);
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 module.exports = app;
