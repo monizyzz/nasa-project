a9aa2a1e5b2d85baaeb76bfa300d8845034b4ab7
bernazyzz <zebernardomoniz@gmail.com>
2025-08-28 12:06:30 +0100

    launches model

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,16 @@
+const lauches = new Map();
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  destination: "Kepler-442 b",
+  costumer: ["NASA", "ZTM"],
+  upcoming: true,
+  success: true,
+};
+
+lauches.set(launch.flightNumber, launch);
+
+module.exports = lauches;
